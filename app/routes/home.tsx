e790c42b42bf65da901dfbdc0a5d657662d1b5f9
ShamirAli55 <shamirali9779@gmail.com>
2025-08-08 08:18:10 -0700

    Puter Integrated

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -11,9 +11,13 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
+export default function Home() 
+{
   return<main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar/>
+
+
+
     <section className="main-section">
       <div className="page-heading py-16">
         <h1>Track Your Applications & Resume Ratings</h1>
